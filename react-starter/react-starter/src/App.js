b44ec4265476884fdfe7d08bd76a33814760a17b
Phuwarin <phuwarin.21@gmail.com>
2019-08-15 10:11:21 +0700

    End Ep.3

--- react-starter/react-starter/src/App.js
+++ react-starter/react-starter/src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
-import { NAME, sayHi } from './sum'
+import queryString from 'query-string'
+
+const Home = () => <p>Home</p>
+const About = () => <p>About</p>
+const Hello = props => {
+  return (
+    <p>
+      Hello, {props.match.params.name}! with query{' : '}
+      {queryString.parse(props.location.search).name}
+    </p>
+  )
+}
 
 class App extends Component {
   render() {
@@ -9,12 +21,27 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Router</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          {NAME} : {sayHi()}
         </p>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/hello/:name" component={Hello} />
+        </Switch>
+
+        <NavLink exact to="/" activeClassName="is-active">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="is-active">
+          About
+        </NavLink>
+        <NavLink to="/hello" activeClassName="is-active">
+          Hello
+        </NavLink>
       </div>
     )
   }
--- react-starter/react-starter/src/sum.js
+++
@@ -1,5 +0,0 @@
-export const NAME = 'Awesome Name'
-
-export function sayHi() {
-    return 'Hello World'
-}

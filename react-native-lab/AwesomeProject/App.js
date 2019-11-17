9754a34912833e6f85f3ad17bb5a9e34f3899d2d
Phuwarin <pang-pond_08@hotmail.com>
2019-11-17 16:18:06 +0700

    Lab PubSub-js

--- react-native-lab/AwesomeProject/App.js
+++ react-native-lab/AwesomeProject/App.js
@@ -1,23 +1,76 @@
 import React, { Component } from 'react';
+import { View, Text, Button, ProgressBarAndroid } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './screens/home-screen';
-import QcScreen from './screens/qc-screen';
+import PubSub from 'pubsub-js';
+
+class HomeScreen extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    };
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            wantRefresh: false
+        }
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, flexDirection: 'column' }}>
+                {
+                    this.state.wantRefresh
+                        ? <ProgressBarAndroid styleAttr="Horizontal" />
+                        : null
+                }
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Home Screen</Text>
+                    <Button
+                        title='Go to Details'
+                        onPress={() => this.props.navigation.navigate('Details')} />
+                </View>
+            </View>
+        );
+    }
+
+    componentDidMount() {
+        let token = PubSub.subscribe('wantRefresh', this.onGlobalEvent.bind(this))
+    }
+
+    onGlobalEvent(message, data) {
+        console.log('[onGlobalEvent] message =', message, ', data =', data)
+        if (message == 'wantRefresh') {
+            this.setState({ wantRefresh: true })
+        }
+    }
+}
+
+class DetailsScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Details'
+    };
+
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Details Screen</Text>
+                <Button
+                    title="Publish"
+                    onPress={() => PubSub.publish('wantRefresh', true)} />
+            </View>
+        );
+    }
+}
 
 const AppNavigator = createStackNavigator(
     {
         Home: HomeScreen,
-        QC: QcScreen
+        Details: DetailsScreen
     },
     {
         initialRouteName: 'Home'
-    });
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends Component {
-    render() {
-        return (
-            <AppContainer />
-        );
     }
-}
+);
+
+export default createAppContainer(AppNavigator);
